283f44349ba9b967c0424a546918582f1567c617
Melissa Armand <melissarmand@gmail.com>
2021-07-19 06:38:45 -0400

    updated routes

--- assets/js/apiRoutes.js
+++ assets/js/apiRoutes.js
@@ -4,6 +4,12 @@ module.exports = (app) => {
     app.get('/api/notes', (req, res) => res.json(noteData));
 
     app.post('/api/notes', (req,res) => {
+        if (noteData.length < 20) {
+            noteData.push(req.body);
+            res.json(true);
+        } else {
+            
+        }
         
     })
 }
--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const fs = require('fs');
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-require('./apiRoutes')(app);
-require('./htmlRoutes') //this needs to be the route;
+require('./assets/js/apiRoutes')(app);
+require('./assets/js/htmlRoutes')(app) //this needs to be the route;
 
 app.listen(PORT, () => {
     console.log(`App is listening on PORT: ${PORT}`);
