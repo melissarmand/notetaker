ef12f0d2be91836aa6ccb4e345cc1739823d22b4
Melissa Armand <melissarmand@gmail.com>
2021-07-23 22:44:17 -0400

    added fs file

--- server.js
+++ server.js
@@ -14,3 +14,9 @@ require('./assets/js/htmlRoutes')(app) //this needs to be the route;
 app.listen(PORT, () => {
     console.log(`App is listening on PORT: ${PORT}`);
 });
+
+fs.readFile(__dirname + '/index.html', function(err, data) {
+    if (err) throw err;
+    res.writeHead(200, { 'Content-Type': 'text/html'});
+    res.end(data);
+});
