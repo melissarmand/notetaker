f0ee12415ad448bbbd8390ce3940e7cb09090c0e
Melissa Armand <melissarmand@gmail.com>
2021-07-29 07:30:43 -0400

    posting method is working

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,13 +1,15 @@
 const noteData = require('../data/noteData');
 const noteDb = require('../db/db.json');
+const fs = require('fs');
 const path = require('path');
 
 module.exports = (app) => {
     app.get('/api/notes', (req, res) => res.json(noteDb));
+        console.log('we are getting it');
     
-    app.post('/api/notes', (req,res) => (noteDb).push(req.body)
-    
-       
+    app.post('/api/notes', (req,res) => res.json(req.body)
+        //console.log('We are posting it')
         
+
     )
 }
