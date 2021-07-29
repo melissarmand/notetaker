94c337491a75582cb6cd703f08f476519be0d60a
Melissa Armand <melissarmand@gmail.com>
2021-07-29 07:39:10 -0400

    fs added to apiroutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -7,7 +7,20 @@ module.exports = (app) => {
     app.get('/api/notes', (req, res) => res.json(noteDb));
         console.log('we are getting it');
     
-    app.post('/api/notes', (req,res) => res.json(req.body)
+    app.post('/api/notes', (req,res) => {
+        var newNote = req.body;
+        
+
+        fs.writeFile('noteDb', JSON.stringify(newNote), function(err) {
+            if (err) throw (err);
+        });
+        console.log('file written to db')
+        
+        res.json(newNote);
+
+
+    
+    }
         //console.log('We are posting it')
         
 
